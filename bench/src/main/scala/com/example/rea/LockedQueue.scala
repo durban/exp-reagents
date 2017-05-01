fd9c2697a7efcfeeff00fd0b613c1bf57031efb3
Daniel Urban <urban.dani@gmail.com>
2017-05-01 11:01:54 +0200

    Better baseline for the queue benchmark

--- bench/src/main/scala/com/example/rea/LockedQueue.scala
+++ bench/src/main/scala/com/example/rea/LockedQueue.scala
@@ -3,14 +3,13 @@ package com.example.rea
 final class LockedQueue[A] {
 
   private[this] val q =
-    new scala.collection.mutable.Queue[A]
+    new java.util.ArrayDeque[A]
 
   def enqueue(a: A): Unit = this.synchronized {
-    q.enqueue(a)
+    q.offer(a)
   }
 
   def tryDequeue(): Option[A] = this.synchronized {
-    if (q.isEmpty) None
-    else Some(q.dequeue())
+    Option(q.poll())
   }
 }
