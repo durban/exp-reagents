082733a7ce4311026fb9c232cdc03dc85af69472
Daniel Urban <urban.dani@gmail.com>
2017-05-07 12:51:19 +0200

    k-CAS benchmark

--- bench/src/main/scala/com/example/rea/kcas/bench/KCASBench.scala
+++ bench/src/main/scala/com/example/rea/kcas/bench/KCASBench.scala
@@ -12,15 +12,22 @@ class KCASBench {
 
   import KCASBench._
 
+  @tailrec
+  private[this] def read(ref: Ref[String], kcas: KCAS): String = {
+    val r = kcas.tryReadOne(ref)
+    if (r eq null) read(ref, kcas)
+    else r
+  }
+
   @Benchmark
   def failedCAS1(r: RefState, t: KCASThreadState): Unit = {
-    val succ = t.kcasImpl.tryPerform(KCASD(CASD(r.ref, r.incorrect, t.nextString()) :: Nil))
+    val succ = t.kcasImpl.tryPerform(KCASD(CASD(r.ref, incorrectOv, t.nextString()) :: Nil))
     if (succ) throw new AssertionError("CAS should've failed")
   }
 
   @Benchmark
   def failedCAS1Baseline(r: RefState, t: CommonThreadState): Unit = {
-    val succ = r.ref.unsafeTryPerformCas(r.incorrect, t.nextString())
+    val succ = r.ref.unsafeTryPerformCas(incorrectOv, t.nextString())
     if (succ) throw new AssertionError("CAS should've failed")
   }
 
@@ -30,9 +37,27 @@ class KCASBench {
     val kcasImpl = t.kcasImpl
     @tailrec
     def go(): Unit = {
-      val ov = kcasImpl.tryReadOne(ref)
+      val ov = read(ref, kcasImpl)
       val nv = (ov.toLong + t.nextLong()).toString
       val succ = kcasImpl.tryPerform(KCASD(CASD(ref, ov, nv) :: Nil))
+      if (succ) ()
+      else go()
+    }
+    go()
+  }
+
+  @Benchmark
+  def successfulKCASLoop(r: KRefState, t: KCASThreadState): Unit = {
+    val refs = r.refs
+    val kcasImpl = t.kcasImpl
+    @tailrec
+    def go(): Unit = {
+      val ds = refs.map { ref =>
+        val ov = read(ref, kcasImpl)
+        val nv = (ov.toLong + t.nextLong()).toString
+        CASD(ref, ov, nv)
+      }
+      val succ = kcasImpl.tryPerform(KCASD(ds))
       if (succ) ()
       else go()
     }
@@ -52,14 +77,38 @@ class KCASBench {
     }
     go()
   }
+
+  @Benchmark
+  def successfulKCASLoopBaseline(r: KRefState, t: CommonThreadState): Unit = {
+    val refs = r.refs
+    @tailrec
+    def goOne(ref: Ref[String]): Unit = {
+      val ov = ref.unsafeTryRead()
+      val nv = (ov.toLong + t.nextLong()).toString
+      val succ = ref.unsafeTryPerformCas(ov, nv)
+      if (succ) ()
+      else goOne(ref)
+    }
+    for (ref <- refs) {
+      goOne(ref)
+    }
+  }
 }
 
 object KCASBench {
 
+  final val incorrectOv = "no such number"
+  final val K = 8
+
   @State(Scope.Benchmark)
   class RefState {
-    private[this] val rnd = ThreadLocalRandom.current()
-    val incorrect = "no such number"
-    val ref = kcas.Ref.mk(rnd.nextLong().toString)
+    val ref = kcas.Ref.mk(ThreadLocalRandom.current().nextLong().toString)
+  }
+
+  @State(Scope.Benchmark)
+  class KRefState {
+    val refs = List.fill(K) {
+      kcas.Ref.mk(ThreadLocalRandom.current().nextLong().toString)
+    }
   }
 }
