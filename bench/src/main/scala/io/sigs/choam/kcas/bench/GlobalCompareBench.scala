d8f9ab7f8a1abf14442837af26020629303c1e5c
Daniel Urban <urban.dani@gmail.com>
2017-10-16 20:30:12 +0200

    Benchmark for Ref.globalCompare

---
+++ bench/src/main/scala/io/sigs/choam/kcas/bench/GlobalCompareBench.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2017 Daniel Urban and contributors listed in AUTHORS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.sigs.choam
+package kcas
+package bench
+
+import org.openjdk.jmh.annotations._
+import org.openjdk.jmh.infra.Blackhole
+
+@Fork(2)
+@Warmup(iterations = 10)
+@Measurement(iterations = 10)
+class GlobalCompareBench {
+
+  import GlobalCompareBench._
+
+  @Benchmark
+  def bench(b: BeSt, bh: Blackhole): Unit = {
+    bh.consume(Ref.globalCompare(b.r1, b.r2))
+  }
+}
+
+object GlobalCompareBench {
+
+  @State(Scope.Benchmark)
+  class BeSt {
+    val r1 = Ref.mk("a")
+    val r2 = Ref.mk("a")
+  }
+}
